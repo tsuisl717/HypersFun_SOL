1c4f5df669f571bae8bdb23448017fb97be56459
Tsuisl717 <tsuisl717@gmail.com>
2026-05-06 17:15:50 +0800

    fix build error.

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,12 +3,10 @@
 import { useMemo } from 'react';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
-import {
-  PhantomWalletAdapter,
-  SolflareWalletAdapter,
-  TorusWalletAdapter,
-  LedgerWalletAdapter,
-} from '@solana/wallet-adapter-wallets';
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-phantom';
+import { SolflareWalletAdapter } from '@solana/wallet-adapter-solflare';
+import { TorusWalletAdapter }    from '@solana/wallet-adapter-torus';
+import { LedgerWalletAdapter }   from '@solana/wallet-adapter-ledger';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
 import { clusterApiUrl } from '@solana/web3.js';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
